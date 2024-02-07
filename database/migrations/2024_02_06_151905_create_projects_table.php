54979103fe1bd3ecb0c65c674d29efc3364fae48
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-07 17:38:11 +0100

    fix: campo slug

--- database/migrations/2024_02_06_151905_create_projects_table.php
+++ database/migrations/2024_02_06_151905_create_projects_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('title', 50)->unique();
-            $table->string('slug', 100)->nullable();
+            $table->string('slug', 100);
             $table->text('description')->nullable();
             $table->timestamps();
         });
