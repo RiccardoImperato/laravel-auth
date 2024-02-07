7bfac4db6efbd591f76c31ee909c3762354df51d
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-07 17:40:24 +0100

    fix: campo slug

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -5,4 +5,5 @@
     <a href="{{ route('admin.projects.edit', $project) }}" class="btn btn-secondary btn-sm mx-2">Modifica</a>
     <h3>{{ $project->title }}</h3>
     <p>{{ $project->description }}</p>
+    <p>{{ $project->slug }}</p>
 @endsection
